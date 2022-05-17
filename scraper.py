e9c68b490e24f000db4405b5fc9664a8081fe913
harry0716 <harry0716@gmail.com>
2022-05-17 20:22:01 +0800

    3.6提升網頁爬蟲效率的非同步技巧

--- scraper.py
+++ scraper.py
@@ -1,15 +1,17 @@
-import requests                #引用requests物件
+import grequests                #引用grequests物件
 from bs4 import BeautifulSoup  #從bs4模組內引用BeautifulSoup物件
+import time                    #引用time模組，為了記錄程式執行時間
 
-for page in range(1,4):    #增加for迴圈，以range方法確定要抓取的page的頁數
+start_time = time.time()
 
-    """第5行至第15行，爬取BBC新聞網站的第一層網頁資料方法"""
-    response = requests.get(
-        f'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt/page/{page}') 
-        #利用get函式抓取網頁，在請求網址的最後，加上page參數。
-        #用f-string方法將for 迴圈中的page變數導入網址列的最後。
+links = [
+    f'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt/page/{page}' for page in range(1,4)]
 
-    soup = BeautifulSoup(response.text,'lxml')  #利用BS套件的抓取網頁內的文字text，也利用lxml
+reqs =  (grequests.get(link) for link in links)      #用grequests.get將抓取到的links依據傳到變數reqs
+resps = grequests.imap(reqs, grequests.Pool(3))      #用grequests.imap將reqs對應到grequests的對應池中pool(3)因為有三個links 
+for index, resp in enumerate(resps):    #此處改用enumerate()及index，為了第37行
+
+    soup = BeautifulSoup(resp.text,'lxml')  #利用BS對抓取的text進行解析
     titles = soup.find_all('span',{'class':'lx-stream-post__header-text gs-u-align-middle'}) #用find_all抓取網頁上所有的標題文字
 
     title_list = []                          #建立一個title_list空串列，為了之後將抓取到的標題文字加入這個空串列
@@ -18,17 +20,23 @@ for page in range(1,4):    #增加for迴圈，以range方法確定要抓取的pa
         
     """爬取BBC新聞網站的子網頁資料方法"""
 
-    urls = soup.find_all('a',{'class':'qa-heading-link lx-stream-post__header-link'}) #用BS的soup方法找出所有對應的a標籤 
+    urls = soup.find_all(
+        'a',{'class':'qa-heading-link lx-stream-post__header-link'}) #用BS的soup方法找出所有對應的a標籤 
+    sub_links = ['http://www.bbc.com' + url.get('href') for url in urls]
+
+    sub_reqs = (grequests.get(sub_link) for sub_link in sub_links)    #用grequests.get將抓取到的sub_links依據傳到變數sub_reqs
+    sub_resps = grequests.imap(sub_reqs, grequests.Pool(10))          #用grequests.imap將reqs對應到grequests的對應池中
+
     tag_list = []
-    for url in urls:
-        sub_response = requests.get('http://www.bbc.com' + url.get('href'))
-        sub_soup = BeautifulSoup(sub_response.text,'lxml')
+    for sub_resp in sub_resps:
+        sub_soup = BeautifulSoup(sub_resp.text,'lxml')
         tags = sub_soup.find_all('li',{'class':'bbc-1msyfg1 e1hq59l0'})
         for tag in tags:
             tag_list.append(tag.getText())  #運用append指令將抓取到的標題文字加入tag_list串列中  
         # print(tag.find('a').getText()) #因為li下面只有一個a標籤，所以程式碼是可以省略.find('a')
-    print(f'第{page}頁')   #用f-string列印頁數
+    print(f'第{index+1}頁')   #參照第12行，for迴圈的索引值從0開始計算
     print(title_list)      #將抓取到的標題列印出來驗證
     print(tag_list)        #將抓取到的子網頁資料列印出來驗證
     
-
+end_time = time.time()
+print(f"花費{end_time - start_time}秒")
